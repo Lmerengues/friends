6267c4bc597bd8e2212f150c033f777c7e6296ce
lmerengues <317958662@qq.com>
2018-05-22 03:32:41 +0800

    v1.0

--- friend.js
+++ friend.js
@@ -5,6 +5,7 @@ var PersonItem = function(text){
         var obj = JSON.parse(text);
         this.title = obj.title;
         this.author = obj.author
+        this.labels = obj.labels
     }
 };
 
@@ -15,6 +16,7 @@ PersonItem.prototype = {
 };
 
 var ThePerson = function () {
+    /*
     LocalContractStorage.defineMapProperty(this, "data", {
         parse: function (text) {
             return new PersonItem(text);
@@ -22,12 +24,24 @@ var ThePerson = function () {
         stringify: function (o) {
             return o.toString();
         }
+    });*/
+    LocalContractStorage.defineMapProperty(this, "arrayMap");
+    LocalContractStorage.defineMapProperty(this, "dataMap",{
+        parse: function (text) {
+            return new PersonItem(text);
+        },
+        stringify: function (o) {
+            return o.toString();
+        }
     });
+
+
+    LocalContractStorage.defineProperty(this, "size");
 };
 
 ThePerson.prototype ={
     init:function(){
-        
+        this.size = 0;
     },
 
     save:function(title,content){
@@ -40,7 +54,7 @@ ThePerson.prototype ={
         }
 
         var from = Blockchain.transaction.from;
-        var personItem = this.data.get(title);
+        var personItem = this.dataMap.get(title);
         if(personItem){
             throw new Error("letter has been occupied");
         }
@@ -48,18 +62,54 @@ ThePerson.prototype ={
         personItem = new PersonItem();
         personItem.author = from;
         personItem.title = title;
+        personItem.labels = {}
 
-        this.data.put(title,personItem);
+        var index = this.size;
+        this.arrayMap.set(index, title);
+        this.dataMap.set(title, personItem);
+        this.size +=1;
+
+        return index
+
+        //this.data.put(title,personItem);
     },
 
     get:function(title){
         if(!title){
             throw new Error("empty title")
         }
-        return this.data.get(title);
+        return this.dataMap.get(title);
+    },
+
+    len:function(){
+        return this.size;
+    },
+
+    comment:function(title,text){
+        if(!title || !text){
+            throw new Error("empty title or content")
+        }
+
+        if(title.length > 20 || text.length>20){
+            throw new Error("title or content  exceed limit length")
+        }
+
+        var from = Blockchain.transaction.from;
+        var personItem = this.dataMap.get(title)
+        var labels = personItem.labels
+        if(text in labels){
+            personItem.labels[text] += 1
+        }
+        else{
+            personItem.labels[text] = 1
+        }
+        this.dataMap.set(title, personItem);
+        return {"info":"success"}
+
     },
 
     forEach: function(limit, offset){
+
         limit = parseInt(limit);
         offset = parseInt(offset);
         if(offset>this.size){
@@ -69,13 +119,19 @@ ThePerson.prototype ={
         if(number > this.size){
             number = this.size;
         }
-        var result  = "";
+        var result  = [];
+
+
         for(var i=offset;i<number;i++){
             var key = this.arrayMap.get(i);
             var object = this.dataMap.get(key);
-            result += "index:"+i+" key:"+ key + " value:" +object+"_";
+            result.push(object)
         }
-        return result;
+        return result
+
+
+
+        return {"a":"b"};
     }
 }
 
